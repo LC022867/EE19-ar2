437b1524a6c4870d9a0610dcd74388492722f8d9
Luciano Cucarano <luciano.cucarano@elev.ga.ntig.se>
2021-02-14 11:47:43 +0100

    Javascript_Train

---
+++ JavaTrain/beginner/beginner.js
@@ -0,0 +1,8 @@
+let age = prompt('What is your age?');
+document.getElementById('someText').innerHTML = age;
+/* Makes a Variable to ask your age. When you put your age it will change from 'Hello' to your age that you will put*/
+
+
+//Numbers in javascript
+let num1 = 5.7;
+console.log(5 * 10);
---
+++ JavaTrain/list.js
@@ -0,0 +1,126 @@
+// <--- comment
+/* <---- my codes
+*/
+
+
+
+
+
+
+
+/*console.log('hello');*/    
+
+//is a function in JavaScript which is used to print any kind of variables defined before in it or to just print any message that needs to be displayed to the user.
+
+
+
+
+/*alert('Hello world');*/ 
+
+//will display text in a dialog box that pops up on the screen.
+
+
+
+
+
+let b = 'something'; //makes a variable
+console.log(b); //put the variable in coonsole and it will write --> something
+
+/*let someNumber = 45;*/ // --> Make a variable
+/*console.log(someNumber);*/ // --> Will write 45
+
+
+
+
+
+
+/*document.getElementById('someText').innerHTML = 'Hello there';*/
+
+//Takes the ID from HTML and chances the text or put it
+
+
+
+
+
+
+/*prompt('What is your age?');*/  
+
+//Puting questions or makes you to answer them
+
+
+
+
+
+//NUMBERS IN JAVASCRPT TUTORIAL 
+
+/*console.log(5 * 10)*/ 
+// 5 * 10 it's 50 like math
+
+/*console.log(50 / 10)*/
+// 50 / 10 it's 5 like math
+
+/*var num1 = 10;
+num1 = num1 + 5;
+console.log(num1);*/
+//What it does is it makes 10 to 15
+
+/*var num1 = 10;
+num1++; 
+console.log(num1);*/
+//num1++ --> num1 + 1;
+//num1-- --> num1 - 1;
+
+
+//Divide, multiply *, remainder %
+/*console.log(num1 % 6);*/
+// 6 only works one time at 10 so it remains 4 left
+
+
+//Increment /decrement by any number you want
+/*let num2 = 10;
+num2 += 20;
+console.log(num2);*/
+//It will be 30, due to 10 + 20 = 30
+// num2 += 20 --> num2 + 20
+
+
+
+
+//FUNCTIONS IN JAVASCRIPT
+
+//This is how you create a function
+
+/*function fun() {
+    alert('this is a fuction')
+}*/
+
+
+
+//How do agruments work in functions?
+//How do we add 2 numbers together in a function?
+
+/*
+function sumNumbers(num1, num2) {
+    let result = num1 + num2; //Make a variable 
+    console.log(result);
+}
+
+sumNumbers(10, 10); */ //Like 10 + 10 = 20
+
+
+
+/*
+function greeting(yourName) {
+    let result = 'Hello' + ' ' + yourName; //String concatenation
+    console.log(result);
+}
+
+let namE = prompt('What is your name?');
+greeting(namE);
+*/
+//Better way to write your code to be more organized 
+
+
+
+
+
