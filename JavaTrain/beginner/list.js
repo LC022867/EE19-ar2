5e62d4f45f106d0cf921264222ace1bf1a8e98d7
Luciano Cucarano <luciano.cucarano@elev.ga.ntig.se>
2021-02-16 13:41:28 +0100

    mylist

---
+++ JavaTrain/beginner/list.js
@@ -0,0 +1,178 @@
+// <--- comment
+/* <---- my codes
+*/
+
+
+
+
+
+
+
+/*
+console.log('hello');
+*/    
+
+//is a function in JavaScript which is used to print any kind of variables defined before in it or to just print any message that needs to be displayed to the user.
+
+
+
+
+/*alert('Hello world');*/ 
+
+//will display text in a dialog box that pops up on the screen.
+
+
+
+
+
+let b = 'something'; //makes a variable
+console.log(b); //put the variable in console and it will write --> something
+
+/*let someNumber = 45;*/ // --> Make a variable
+/*console.log(someNumber);*/ // --> Will write 45
+
+
+
+
+
+
+/*document.getElementById('someText').innerHTML = 'Hello there';*/
+
+//Takes the ID from HTML and chances the text or put it in
+
+
+
+
+
+
+/*prompt('What is your age?');*/  
+//Puting questions or makes you to answer them
+
+
+
+
+
+
+
+//NUMBERS IN JAVASCRPT TUTORIAL 
+
+/*console.log(5 * 10)*/ 
+// 5 * 10 it's 50 like math
+
+/*console.log(50 / 10)*/
+// 50 / 10 it's 5 like math
+
+/*
+var num1 = 10;
+num1 = num1 + 5;
+console.log(num1);
+*/
+//What it does is it makes 10 to 15
+
+/*
+var num1 = 10;
+num1++; 
+console.log(num1);
+*/
+//num1++ --> num1 + 1;
+//num1-- --> num1 - 1;
+
+
+//Divide, multiply *, remainder %
+/*
+console.log(num1 % 6);
+*/
+// 6 only works one time at 10 so it remains 4 left
+
+
+//Increment /decrement by any number you want
+/*
+let num2 = 10;
+num2 += 20;
+console.log(num2);
+*/
+//It will be 30, due to 10 + 20 = 30
+// num2 += 20 --> num2 + 20
+
+
+
+
+
+
+//FUNCTIONS IN JAVASCRIPT
+
+//This is how you create a function
+
+/*function fun() {
+    alert('this is a fuction')
+}*/
+
+
+
+//How do agruments work in functions?
+//How do we add 2 numbers together in a function?
+
+/*
+function sumNumbers(num1, num2) {
+    let result = num1 + num2; //Make a variable 
+    console.log(result);
+} 
+
+sumNumbers(10, 10); 
+*/ 
+//Like 10 + 10 = 20
+
+
+
+/*
+function greeting(yourName) { // changes namE to yourname 
+    let result = 'Hello' + ' ' + yourName; //String concatenation
+    console.log(result); //Gets the results
+}
+
+let namE = prompt('What is your name?');
+greeting(namE);
+*/
+//Better way to write your code to be more organized 
+
+
+
+
+
+//WHILE VS FOR PS IN JAVASCRIPT
+
+//WHILE LOOPS
+
+/* 
+let num = 0;
+
+while (num < 100) { //goes on until it gets to 100
+    num += 1;
+    console.log(num);
+}
+//Prints out 1 to 100 and stops
+*/
+
+
+//FOR LOOP
+
+/*
+for (let num = 0; num <= 100; num++) { //goes on until it gets to 100
+    console.log(num);
+}
+*/
+
+//While and for makes the same task. However for loop makes it more clean.
+//Should use while or for when you want to do it multiple times.
+
+
+
+
+//DATATYPES
+/*
+let yourAge = 18; //number
+let yourName = 'Luciano'; //string
+let name = {first: 'Jane', last: 'Doe'}; //Object
+let truth = false; //boolean
+let groceries = ['apple', 'banana', 'oranges']; //array
+*/
