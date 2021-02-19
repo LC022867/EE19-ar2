35f3fbbc7b11dbb13fbb03c7a5f1f02f5ffcd17d
Luciano Cucarano <luciano.cucarano@elev.ga.ntig.se>
2021-02-19 15:14:14 +0100

    Variables and vaules

--- udemy/01-Fundamentals-Part-1/starter/script.js
+++ udemy/01-Fundamentals-Part-1/starter/script.js
@@ -7,3 +7,25 @@ if (js == 'amazing')
 40 + 8 + 23 - 10 //Will not print out in the console
 
 console.log(40 + 8 + 23 - 10); // Console.log whenever you want to access the developer console
+
+//Values and variables 
+
+console.log("Jonas"); //Jonas is the value
+console.log(23); //Vaule is the smallest unit of information in javascript
+
+
+let firstName = "Luciano"; //creating a variable in your computers memory
+//First box "firstname" which inside is "Jonas"
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);  // Write the varible instead of passing a value like "Luciano"
+
+//You can't start to name a variable with an uppercase, nummber and name codes like Function, new and so on...
+
+//Name a variable that makes the programmer easier to understand
+
+
+
+
+
