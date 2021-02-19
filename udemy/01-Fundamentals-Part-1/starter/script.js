2faa86f5a680a060ae53f841ab59029e021cc599
Luciano Cucarano <luciano.cucarano@elev.ga.ntig.se>
2021-02-19 11:55:44 +0100

    fundamentals part 1 Basic javascript

---
+++ udemy/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,9 @@
+//This script is called an inline script
+
+let js = 'amazing';
+if (js == 'amazing')
+    alert('Javascript is FUN!');
+
+40 + 8 + 23 - 10 //Will not print out in the console
+
+console.log(40 + 8 + 23 - 10); // Console.log whenever you want to access the developer console
