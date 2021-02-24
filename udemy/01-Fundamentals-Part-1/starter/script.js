e4973fcfe0c4da09422680e32969585d2af6dc1d
Luciano Cucarano <luciano.cucarano@elev.ga.ntig.se>
2021-02-24 09:59:10 +0100

    Data types

--- udemy/01-Fundamentals-Part-1/starter/script.js
+++ udemy/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,5 @@
 //This script is called an inline script
-
+/*
 let js = 'amazing';
 if (js == 'amazing')
     alert('Javascript is FUN!');
@@ -7,9 +7,12 @@ if (js == 'amazing')
 40 + 8 + 23 - 10 //Will not print out in the console
 
 console.log(40 + 8 + 23 - 10); // Console.log whenever you want to access the developer console
+*/
+
+//////////////////////////////////////////
 
 //Values and variables 
-
+/*
 console.log("Jonas"); //Jonas is the value
 console.log(23); //Vaule is the smallest unit of information in javascript
 
@@ -20,12 +23,19 @@ let firstName = "Luciano"; //creating a variable in your computers memory
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);  // Write the varible instead of passing a value like "Luciano"
-
+*/
 //You can't start to name a variable with an uppercase, nummber and name codes like Function, new and so on...
 
 //Name a variable that makes the programmer easier to understand
 
+////////////////////////////////////////////
 
 
+//DATA TYPES
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);;
+
+console.log(typeof true); //This operator "typeof" will make a new value which is a string with the type of this value. It will be "Boolean"
 
 
