f671dd29b6d8d843c85174f872df8bb420853f38
Luciano Cucarano <luciano.cucarano@elev.ga.ntig.se>
2021-03-09 23:00:33 +0100

    inte klar än

---
+++ Javascript/summa/summa.js
@@ -0,0 +1,12 @@
+const knappEtt = document.querySelector(".tal1");
+const knappTvå = document.querySelector(".tal2");
+const inputPlus = document.querySelector(".plus");
+const inputMinus = document.querySelector(".minus");
+const summa = document.querySelector(".summa");
+
+var antalPlus = knappEtt + knappTvå;
+var antalMinus = knappEtt - knappTvå;
+
+
+
+
