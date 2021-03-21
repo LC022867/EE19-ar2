14533b0778031c0a0aff137344eeb1db05669c7d
Luciano Cucarano <luciano.cucarano@elev.ga.ntig.se>
2021-03-21 22:31:27 +0100

    Challenge 2 if/else

--- udemy/01-Fundamentals-Part-1/starter/script.js
+++ udemy/01-Fundamentals-Part-1/starter/script.js
@@ -226,4 +226,38 @@ if (birthDay <= 2000) {
 }
 console.log(century);
 */
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 
+//CODING CHALLENGE 2
+/*
+Use the BMI example from Challenge #1, and the code you already wrote, and
+improve it.
+
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
+Hint: Use an if/else statement
+GOOD LUCK!
+*/
+
+//My test code
+/*
+const marksHeight = 1.95;
+const makrsMass = 90;
+const johnHeight = 1.76;
+const johnMass = 85;
+
+const m_BMI = makrsMass / marksHeight ** 2;
+const j_BMI = johnMass / (johnHeight * johnHeight);
+const markHigherBMI = m_BMI > j_BMI;
+
+if (markHigherBMI) {
+    console.log(`Mark's BMI ${m_BMI} is higher than John's ${j_BMI}!`);
+} else {
+    console.log(`John's BMI ${j_BMI} is higher than Mark's ${m_BMI}!`);
+}
+*/
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
