aa8ae27d4b613931595ff7d9f3f5d5c4091889c4
Luciano Cucarano <luciano.cucarano@elev.ga.ntig.se>
2021-04-09 19:25:18 +0200

    assignment functions javascript

---
+++ udemy/02-Fundamentals-Part-2/starter/assignment2.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//FUNCTIONS
+/*
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+
+const sweden = describeCountry('Sweden', 10, 'Stockholm');
+console.log(sweden);
+
+const finland = describeCountry('Finland', 5.5, 'Helsink')
+console.log(finland);
+
+const italy = describeCountry('Italy', 60, 'Rom');
+console.log(italy);
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
