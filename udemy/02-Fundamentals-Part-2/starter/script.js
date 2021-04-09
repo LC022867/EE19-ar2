64ae0816ed28322231fa186f6008c2a99d8e1fc8
Luciano Cucarano <luciano.cucarano@elev.ga.ntig.se>
2021-04-09 19:09:06 +0200

    function javascript

--- udemy/02-Fundamentals-Part-2/starter/script.js
+++ udemy/02-Fundamentals-Part-2/starter/script.js
@@ -29,3 +29,35 @@ const interface = 'Audio';
 const private = '534';
 */
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//FUNCTIONS
+
+/*
+//All the functions doesn't need to return something
+function logger() {
+    console.log('My name is Jonas');
+}
+
+// calling / running / invoking function
+logger();
+
+
+//parameters are like variables that are specific only to this function and they all get defined ones we call the function. (represents the input data of this function)
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    //We can return any value from the function
+    return juice;
+}
+
+//If we want to use that value that was returned. We need to store it in a variable.
+//The parameters are called agruments
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+//We can use as many times as we wanted
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+//We use return if we want to use over and over again
+*/
