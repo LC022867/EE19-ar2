0f7645d2dca945748f38865929ca15473cfd25f9
Luciano Cucarano <luciano.cucarano@elev.ga.ntig.se>
2021-04-13 23:46:23 +0200

    comments about the code music

--- training/music/music.js
+++ training/music/music.js
@@ -1,12 +1,13 @@
+
 const mySong = document.getElementById("No_Game_No_Life");
 const icon = document.getElementById("icon");
 
-icon.onclick = function () {
-    if (mySong.paused) {
+icon.onclick = function () { //When someone clicks the icon
+    if (mySong.paused) { //If the music is paused, then play 
         mySong.play();
-        icon.src = "./pictures/pause.png";
+        icon.src = "./pictures/pause.png"; //Change the icon to pause symbol
     } else {
-        mySong.pause();
-        icon.src = "./pictures/play.png";
+        mySong.pause(); //If the music is playing, then pause
+        icon.src = "./pictures/play.png"; //Change the icon to play symbol
     }
 }
